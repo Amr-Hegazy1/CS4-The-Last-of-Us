5f95ad8e4c409c80b95373ad3713091c34bbc713
unknown <omarsaqr755@gmail.com>
2023-05-04 16:28:33 +0300

    game engine win & start

--- src/engine/Game.java
+++ src/engine/Game.java
@@ -2,8 +2,11 @@ package engine;
 
 
 import java.util.*;
+import java.awt.*;
 
 import model.characters.*;
+import model.collectibles.Supply;
+import model.collectibles.Vaccine;
 import model.world.*;
 
 import java.io.*;
@@ -89,6 +92,82 @@ public class Game {
 //		map = new Cell[15][15];
 		
 	}
+	public static Point generateRandomLoaction() {
+		Random rand = new Random();
+		
+		int randomX = rand.nextInt(15);
+		int randomY = rand.nextInt(15);
+		
+		Cell[][] map = Game.getMap();
+		
+		while(map[randomX][randomY] instanceof TrapCell || map[randomX][randomY] instanceof CollectibleCell || (map[randomX][randomY] instanceof CharacterCell && ( (CharacterCell) map[randomX][randomY] ).getCharacter() != null) ) {
+			
+			randomX = rand.nextInt(15);
+			randomY = rand.nextInt(15);
+			
+		}
+		
+		return new Point(randomX,randomY);
+		
+	}
+	public static void startGame(Hero h) throws Exception {
+		for (int i=0;i<15;i++) {
+			for(int j=0;j<15;j++) {
+				map[i][j] =new CharacterCell(null);
+				
+			}
+		}
+		loadHeroes("Heroes.csv");
+	    availableHeroes.remove(h);
+		heroes.add(h);
+		map[14][0]= new CharacterCell(h);
+		for (int k=0;k<10;k++) {
+			Point p= generateRandomLoaction();
+			int x=(int) p.getX();
+			int  y=(int) p.getY();
+			map[x][y]=new CharacterCell(new Zombie());
+		}
+		for(int i=0;i<5;i++) {
+			Point p= generateRandomLoaction();
+			int x=(int) p.getX();
+			int  y=(int) p.getY();
+			map[x][y]=new CollectibleCell(new Vaccine());
+			 p= generateRandomLoaction();
+			 x=(int) p.getX();
+			  y=(int) p.getY();
+			map[x][y]=new CollectibleCell(new Supply());
+			 p= generateRandomLoaction();
+			 x=(int) p.getX();
+			  y=(int) p.getY();
+			map[x][y]=new TrapCell();
+			
+		}
+	}
+	
+	public static boolean checkWin() {
+		int heroalive = heroes.size();
+		if(heroalive <5)
+			return false;
+		else {
+			for(int i=0 ; i<heroalive ;i++) {
+				Hero h = heroes.get(i);
+				if(h.getVaccineInventory()!=null)
+					return false;
+				for(int k = 0 ; k < 15 ; k++) 
+					for(int j = 0 ; j < 15 ; j++) {
+						if(map[k][j] instanceof CollectibleCell) {
+							CollectibleCell c = (CollectibleCell) map[k][j];
+							if(c.getCollectible() instanceof Vaccine)
+								return false;
+						}
+					}
+			
+		} 
+			return true;
+		}
+		
+		 
+	}
 	
 	public static void loadHeroes(String filePath) throws Exception {
 		
@@ -116,7 +195,7 @@ public class Game {
 			
 			
 			}
-			
+		
 			
 			availableHeroes.add(hero);
 			
